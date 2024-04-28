71d6fe0fe5c56d507b12f4014eb349901ca2eb07
aidamagoudiop <aidamagoudiop40@gmail.com>
2024-04-28 14:19:11 +0000

    deuxieme app movie, connexion express+mongodb

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -49,3 +49,94 @@ exports.findAll = (req, res) => {
         });
       });
   };
+
+exports.findOne =  (req, res) => {
+    const id = req.params.id;
+    Movie.findById(id)
+      .then(data => {
+        if (!data)
+        res.status(404).send({ message: "Not found Movie with id" + id
+      });
+      else res.send(data);
+    })
+    
+}
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: 
+      "Data to update can not be empty!",
+    }); 
+  }
+  const id = req.params.id;
+  Movie.findByIdAndUpdate(id, req.body,
+  { useFindAndModify: false })
+    .then(data => {
+      if(!data) {
+        res.status(404).send({ message: 
+          `Cannot update Movie with id=${id}. Maybe Movie was not found`,
+        }); 
+      } else res.send({ message: 
+        "Movie was updated successfully"});
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Movie with id=" +id,
+      });
+    });
+};
+
+exports.delete =  (req, res) => {
+  const id = req.params.id;
+  Movie.findByIdAndRemove(id)
+    .then(data => {
+      if (!data)
+      res.status(404).send({ message: 
+        `Cannot delete Movie with id=${id}. Maybe Movie was not found`,
+    });
+    else {
+      res.send({ 
+        message: 
+        "Movie was deleted successfully",
+      });      
+    }
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: "Could not delete Movie with id=" +id,
+    });
+  });
+  
+};
+
+
+exports.deleteAll =  (req, res) => {
+  Movie.deleteMany({})
+    .then(data => {
+      res.send({ 
+        message: 
+        `${data.deletedCount}
+        Students were deleted successfully`,
+      });      
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: 
+      err.message || "Some error occurred while removing all students.",
+    });
+  });
+  
+};
+
+exports.findAllRegistered =  (req, res) => {
+  Movie.find({ registered: true })
+    .then(data => {
+      res.send(data);      
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: 
+      err.message || "Some error occurred while retrieving students.",
+    });
+  });
+  
+};
